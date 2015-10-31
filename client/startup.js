db840f8515ffce9a71e9a0000c6a9f6379919d87
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2015-10-31 00:17:34 -0700

    Highlight items that are in shopping list.

--- client/startup.js
+++ client/startup.js
@@ -3,8 +3,17 @@ Meteor.startup(() => {
   Tracker.autorun(() => {
     let userId = Meteor.userId();
 
-    Meteor.subscribe("items", userId);
-    Meteor.subscribe("shoppinglist", userId);
+    let itemsHandle = Meteor.subscribe("items", userId);
+    let shoppingListHandle = Meteor.subscribe("shoppinglist", userId);
+
+    if (Meteor.userId() &&
+        itemsHandle.ready() &&
+        shoppingListHandle.ready()) {
+
+      ShoppingList.find({userId}).forEach((sl) => {
+        $(`#allitems li[data-name="${sl.name}"]`).addClass("list-group-item-success");
+      });
+    }
   });
 
 });
