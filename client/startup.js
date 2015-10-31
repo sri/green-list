5e3e520445dec368ecb6f7f9c37159c992f7c0a1
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2015-10-31 13:50:50 -0700

    Consider shopping list items that are removed

--- client/startup.js
+++ client/startup.js
@@ -10,6 +10,12 @@ Meteor.startup(() => {
         itemsHandle.ready() &&
         shoppingListHandle.ready()) {
 
+      // We want to highlight items in All Items
+      // that are in the shopping list. It needs
+      // to be reactive -- items can get added
+      // or removed from the shopping list anytime.
+      $("#allitems li.list-group-item-success").removeClass("list-group-item-success");
+
       ShoppingList.find({userId}).forEach((sl) => {
         $(`#allitems li[data-name="${sl.name}"]`).addClass("list-group-item-success");
       });
